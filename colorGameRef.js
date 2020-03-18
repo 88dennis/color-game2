9412dd87e4123854a5de975d0f252edf23cba5eb
Dennis Sarmiento <dennissarmiento8080@gmail.com>
2020-03-18 09:28:17 +0800

    up

--- colorGameRef.js
+++ colorGameRef.js
@@ -1,12 +1,3 @@
-// let colors = [
-//     "rgb(255, 0, 0)",
-//     "rgb(255, 255, 0)",
-//     "rgb(255, 255, 255)",
-//     "rgb(255, 0, 255)",
-//     "rgb(0, 255, 255)",
-//     "rgb(0, 0, 255)",
-// ];
-
 //make a variable to store and track the value; to be used in the hard and easy mode
 let numSquares = 6;
 let colors = generateRandomColorsArr(numSquares);
@@ -23,100 +14,41 @@ let h1 = document.querySelector("h1");
 let hardBtn = document.querySelector("#hardBtn");
 let modeBtns = document.getElementsByClassName("mode");
 
-hardBtn.classList.add("selected");
 
-for(let i = 0; i < modeBtns.length; i++) {
-    console.log(modeBtns);
+init();
+function init(){
+    hardBtn.classList.add("selected");
+    setUpModeBtns();
+    setUpSquares();
+    reset();
+}
+
+resetBtn.addEventListener("click", function(){
+    reset();
+    messageDisplay.textContent="Guess what color is the RGB below...";
+    h1.style.backgroundColor = "steelblue";
+    this.textContent = "New Colors"
+})
+
+function setUpModeBtns(){
+ //MODE BUTTONS
+ for(let i = 0; i < modeBtns.length; i++) {
     modeBtns[i].addEventListener("click", function(){
         removeSelectedClass();
         this.classList.add("selected");
-
         if(modeBtns[i].textContent === "EASY") {
             numSquares = 3;
         } else if(modeBtns[i].textContent === "HARD") {
             numSquares = 6;
         }
         reset();
-        //how many squares to show
-        //pick new colors
-        //update page to reflect changes
-    })
+        }) 
+    }
 }
 
-function reset() {
-    colors = generateRandomColorsArr(numSquares);
-    console.log(colors)
-    colorToGuess = guessThisColor();
-    colorDisplay.textContent = colorToGuess;
-
-    for(let i = 0; i < squares.length; i++) {
-        if(colors[i]){
-            //add initial colors to squares
-            squares[i].style.backgroundColor = colors[i];
-            squares[i].style.display = "inline-block";
-        } else {
-            squares[i].style.display = "none";
-        }
-    }
-    messageDisplay.textContent="Guess what color is the RGB below...";
-    h1.style.backgroundColor = "#232323";
-    this.textContent = "New Colors"
-}
-
-//HIDE THE THREE BOXES AND COLOR THE OTHER 3 BOXES
-// easyBtn.addEventListener("click", function(){
-//     hardBtn.classList.remove("selected");
-//     easyBtn.classList.add("selected")
-//     numSquares = 3;
-//     colors = generateRandomColorsArr(numSquares);
-//     colorToGuess = guessThisColor();
-//     colorDisplay.textContent = colorToGuess;
-//     console.log(colors)
-
-//     for( let i = 0; i < squares.length; i++){
-//         if(colors[i]) {
-//             squares[i].style.backgroundColor = colors[i];
-//         } else {
-//             squares[i].style.display = "none"
-//         }
-//     } 
-// })
-
-// hardBtn.addEventListener("click", function(){
-//     easyBtn.classList.remove("selected");
-//     hardBtn.classList.add("selected")
-//     numSquares = 6;
-//     colors = generateRandomColorsArr(numSquares);
-//     colorToGuess = guessThisColor();
-//     colorDisplay.textContent = colorToGuess;
-//     console.log(colors)
-    
-//     for( let i = 0; i < squares.length; i++){
-//             squares[i].style.backgroundColor = colors[i];
-//             squares[i].style.display = "inline-block"
-//     } 
-// })
-
-resetBtn.addEventListener("click", function(){
-    colors = generateRandomColorsArr(numSquares);
-    console.log(colors)
-    colorToGuess = guessThisColor();
-    colorDisplay.textContent = colorToGuess;
-    for(let i = 0; i < squares.length; i++) {
-        //add initial colors to squares
-        squares[i].style.backgroundColor = colors[i];
-        squares[i].style.display = "inline-block";
-    }
-    messageDisplay.textContent="Guess what color is the RGB below...";
-    h1.style.backgroundColor = "steelblue";
-    this.textContent = "New Colors"
-})
-
-colorDisplay.textContent = colorToGuess;
-//fill in the squares with colors
-for(let i = 0; i < squares.length; i++) {
-    //add initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
+function setUpSquares() {
+  //fill in the squares with colors
+  for(let i = 0; i < squares.length; i++) {
     //add event listener
     squares[i].addEventListener("click", function(){
         let clickedColor = this.style.backgroundColor;
@@ -137,6 +69,8 @@ for(let i = 0; i < squares.length; i++) {
     })
 };
 
+    
+}
 //FUNCTION TO CHANGE THE COLORS OF THE SQUARES IF CORRECT
 function changeColors(color){
     for( let i = 0; i < colors.length; i++) {
@@ -169,9 +103,28 @@ function randomColor() {
     return rgbText;
 }
 
-//TO REMOVE THE SELECTED CLASS ON THE BUTTONS
+//TO REMOVE THE SELECTED CLASS ON THE BUTTON    S
 function removeSelectedClass(){
     for(let i = 0; i < modeBtns.length; i++) {
         modeBtns[i].classList.remove("selected");
     }
+}
+
+function reset() {
+    colors = generateRandomColorsArr(numSquares);
+    console.log(colors)
+    colorToGuess = guessThisColor();
+    colorDisplay.textContent = colorToGuess;
+    for(let i = 0; i < squares.length; i++) {
+        if(colors[i]){
+            //add initial colors to squares
+            squares[i].style.display = "inline-block";
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    messageDisplay.textContent="Guess what color is the RGB below...";
+    h1.style.backgroundColor = "#232323";
+    this.textContent = "New Colors"
 }
